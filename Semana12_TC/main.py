ded48b2042405b48ce734aa5d36c350174f033ad
Carlos Juca <chjuca@utpl.edu.ec>
2019-01-04 13:44:06 -0500

    Agregando Actividad en Clase #12

--- Semana12_TC/main.py
+++ Semana12_TC/main.py
@@ -1,26 +1,27 @@
-from modelado.mimodelo import *
-m = MiArchivo()
-m2 = MiArchivoEscribir()
+from modelado.mimodelo import *						# Se importa todas las clases del modulo mimodelo
+m = MiArchivo()										# Creamos un objeto tipo Archivo
+m2 = MiArchivoEscribir()							# Creamos un Objeto tipo MiArchivoEscribir
 
-lista = m.obtener_informacion()
-lista = [l.split("|") for l in lista]
+lista = m.obtener_informacion()						# Creamos una lista con la infoamcion leida
+lista = [l.split("|") for l in lista]				# Separamos la lista por el caracter "|"
 	
-lista_equipos=[]
-for i in lista:
-    e = Equipo(i[0], i[1], i[2], i[3])
-    lista_equipos.append(e)
+lista_equipos=[]								# Creamos uns lista vacia
+for i in lista:									
+    e = Equipo(i[0], i[1], i[2], i[3])			# Creamos un Objeto de tipo Equipo
+    lista_equipos.append(e)						# Añadimos un elemento al final de la lista 
 
-op = Operaciones(lista_equipos)
+op = Operaciones(lista_equipos)					# Creamos un objeto tipo Operacion  
 
-opt=input("Desea Ordenar la lista de Equipos por Nombre o Campeonatos(N/C)?  ")
-if opt=='N' or opt=='n':
-	lista_ordenada=op.ordenarNombre()
+opt=input("Desea Ordenar la lista de Equipos por Nombre o Campeonatos(N/C)?  ")		# Mostramos un mensaje en pantalla
+
+if opt=='N' or opt=='n':						# Condicion para evaluar el valor ingresado
+	lista_ordenada=op.ordenarNombre()			# Se llama a la funcion OdernarNombre
 else:
-	lista_ordenada=op.ordenarCampeonatos()
+	lista_ordenada=op.ordenarCampeonatos()		# Se llama a la funcion OrdenarCampeonatos
 
 for i in lista_ordenada:
-	m2.agregar_informacion(i)
-	print(i)
+	m2.agregar_informacion(i)					# Se agrega el objeto a la lista
+	print(i)									# Se presenta la infoamcion del Objeto
 
-m.cerrar_archivo()
+m.cerrar_archivo()								# Cerramos ambos archivos
 m2.cerrar_archivo()
--- Semana12_TC/modelado/mimodelo.py
+++ Semana12_TC/modelado/mimodelo.py
@@ -1,14 +1,14 @@
-import codecs
+import codecs								# Importamos las librerias Ncesarias
 import sys
-
-class Equipo(object):
-	def __init__(self, nombres,ciudad,campeonatos,numero):
-		self.nombres = nombres
+		
+class Equipo(object):						# Creamo una Clase Equipo
+	def __init__(self, nombres,ciudad,campeonatos,numero):		#Constructor de la Clase Equipo	quipo
+		self.nombres = nombres									# Atributos de la clase
 		self.ciudad = ciudad
 		self.campeonatos = int(campeonatos)
 		self.numJugadores = int(numero)
 
-	def setNombre(self,nombres):
+	def setNombre(self,nombres):						# Metodo set de la Clase
 		self.nombres = nombres
 
 	def setCiudad(self,ciudad):
@@ -20,7 +20,7 @@ class Equipo(object):
 	def setNumJugadores(self,numero):
 		self.numJugadores = int(numero)
 
-	def getNombre(self):
+	def getNombre(self):							# Metodos get de la Clase
 		return self.nombres
 
 	def getCiudad(self):
@@ -32,41 +32,41 @@ class Equipo(object):
 	def getNumJugadores(self):
 		return self.numJugadores
 
-	def __str__(self):
+	def __str__(self):							# Metodo __str__para presentar la informacion de la clase
 		return "- Nombre: %s\n-Ciudad: %s\n-Campeonatos: %d\n-Numero de Jugadores: %d\n\n"%(self.getNombre(),self.getCiudad(),self.getCampeonatos(),self.getNumJugadores())
 
-	def __repr__(self):
+	def __repr__(self):							# Metodo __repr__ para presentar la informacion frente a otra funcion 
 		return "- Nombre: %s\n-Ciudad: %s\n-Campeonatos: %d\n-Numero de Jugadores: %d\n\n"%(self.getNombre(),self.getCiudad(),self.getCampeonatos(),self.getNumJugadores())
 
 
-class MiArchivo():
-	def __init__(self):
-		self.archivo = codecs.open("informacion.csv","r")
+class MiArchivo():						# Clase MiArchivo
+	def __init__(self):					# Constructor de la clase
+		self.archivo = codecs.open("informacion.csv","r")	# archivo abre el documento csv en modo read
 
-	def obtener_informacion(self):
+	def obtener_informacion(self):			# Se lee las lineas del arrchivo
 		return self.archivo.readlines()
 
-	def cerrar_archivo(self):
+	def cerrar_archivo(self):				# Se cierra el Archivo
 		self.archivo.close()
 
-class MiArchivoEscribir:
+class MiArchivoEscribir():				# Creacion de la clase MiArchivoEscribir
 
-    def __init__(self):
-        self.archivo = codecs.open("informacion_ordenada.csv", "a")
+    def __init__(self):					# Constructor de la clase
+        self.archivo = codecs.open("informacion_ordenada.csv", "a")	  # archivo abre el documento csv en modo write
 
-    def agregar_informacion(self, p):
-        self.archivo.write("- Nombre: %s\n-Ciudad: %s\n-Campeonatos: %d\n-Numero de Jugadores: %d\n\n"%(p.getNombre(),p.getCiudad(),p.getCampeonatos(),p.getNumJugadores()))
+    def agregar_informacion(self, p):				# Metodo que permite la escritura en un nuevo documento
+        self.archivo.write("-Nombre: %s\n-Ciudad: %s\n-Campeonatos: %d\n-Numero de Jugadores: %d\n\n"%(p.getNombre(),p.getCiudad(),p.getCampeonatos(),p.getNumJugadores()))
 
-    def cerrar_archivo(self):
+    def cerrar_archivo(self):				# Metodo que permite cerrar el archivo
         self.archivo.close()
 
-class Operaciones(object):
+class Operaciones(object):					# Creacion de la clase Operaciones
     
-    def __init__(self, listado):
-        self.listado_equipos = listado
+    def __init__(self, listado):			# Constructor de la clase Operaciones
+        self.listado_equipos = listado 		# Atributo de la clase
 
-    def ordenarNombre(self):
+    def ordenarNombre(self):				# Metoodo que permite Ordenar la lista de objetos segun el Nombre
         return sorted(self.listado_equipos, key=lambda e: e.getNombre())
 
-    def ordenarCampeonatos(self):
+    def ordenarCampeonatos(self):			# Metoodo que permite Ordenar la lista de objetos segun los Campeonatos
         return sorted(self.listado_equipos, key=lambda e: e.getCampeonatos())
